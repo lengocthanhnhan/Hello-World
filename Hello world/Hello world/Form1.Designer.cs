1a3eb6780c4a5435ce313ec1951c5b61fad26324
nhan181197 <nhan181197@gmail.com>
2017-09-19 17:42:51 +0700

    Hello World

---
+++ Hello world/Hello world/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Hello_world
+{
+    partial class frmHelloWorld
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHelloWorld = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHelloWorld
+            // 
+            this.lblHelloWorld.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.lblHelloWorld.AutoSize = true;
+            this.lblHelloWorld.BackColor = System.Drawing.SystemColors.Control;
+            this.lblHelloWorld.Font = new System.Drawing.Font("Monotype Corsiva", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHelloWorld.ForeColor = System.Drawing.Color.Green;
+            this.lblHelloWorld.Location = new System.Drawing.Point(306, 70);
+            this.lblHelloWorld.Name = "lblHelloWorld";
+            this.lblHelloWorld.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblHelloWorld.Size = new System.Drawing.Size(0, 57);
+            this.lblHelloWorld.TabIndex = 0;
+            this.lblHelloWorld.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblHelloWorld.UseWaitCursor = true;
+            this.lblHelloWorld.Click += new System.EventHandler(this.lblHelloWorld_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.AutoSize = true;
+            this.btnShow.BackColor = System.Drawing.SystemColors.Control;
+            this.btnShow.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.ForeColor = System.Drawing.Color.Green;
+            this.btnShow.Location = new System.Drawing.Point(256, 229);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btnShow.Size = new System.Drawing.Size(148, 33);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Hello World";
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // frmHelloWorld
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 274);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lblHelloWorld);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "frmHelloWorld";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHelloWorld;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ Hello world/Hello world/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hello_world
+{
+    public partial class frmHelloWorld : Form
+    {
+        public frmHelloWorld()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblHelloWorld_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "Hello World";
+        }
+    }
+}
